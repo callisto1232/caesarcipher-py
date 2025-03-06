560c13410ddfd6e219386e07294eb0cd532c46df
callisto1232 <146484672+callisto1232@users.noreply.github.com>
2025-03-06 22:57:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils.encrypt import Encrypt
 from utils.decrypt import Decrypt
-print("hi")
-word_l = (Decrypt("krwod").decrypt())
-word = "".join(i for i in word_l)
-print(word)
+encrypted = Encrypt("example", 5).encrypt() # encrypting word with shift 5
+decrypted_l = Decrypt("jcfruqj").decrypt() # decrypting the encrypted word
+decrypted = "".join(i for i in decrypted_l) # converting list to string
+print("encrypted:", encrypted, "decrypted:", decrypted)
