b32fca1d7b0bcaf83a88479a4a47021c1d539d0a
callisto1232 <146484672+callisto1232@users.noreply.github.com>
2025-03-06 22:45:04 +0300

    Add files via upload

--- decrypt.py
+++ decrypt.py
@@ -1,38 +1,29 @@
 import json
 import string
-alphabet = list(string.ascii_lowercase)
-with open("word.json") as file:
-    data = json.load(file)
-
-def decrypt(encrypted_word, shift):
-    decrypted = ""
-    encrypted = encrypted_word.lower().strip()
-    encrypted_word = list(encrypted)
-    for character in encrypted_word:
-        if character in alphabet:
-            c_index = alphabet.index(character)
-            new_char = alphabet[(c_index - shift) % 26]
-            decrypted += new_char
-        else:
-            decrypted += character
-    return decrypted
-
-def main():
-    try:
-        encrypted_word = input('Please enter the encrypted word: ')
+class Decrypt:
+    alphabet = list(string.ascii_lowercase)
+    with open("utils/word.json") as file:
+        data = json.load(file)
+    def __init__(self, encrypted):
+        self.encrypted = encrypted
+    def decrypt(self):
+        encrypted = self.encrypted
+        data = self.data
+        alphabet = self.alphabet
         found_words = []
+        self.encrypted = encrypted
+        encrypted = encrypted.lower().strip()
+        encrypted_word = list(encrypted)
         for shift in range(26):
-            decrypted_word = decrypt(encrypted_word, shift)
-            if decrypted_word in data:
-                found_words.append(decrypted_word)
-                print(f'Found matching word: {decrypted_word} with seed {shift}')
+            decrypted = ""
+            for character in encrypted_word:
+                if character in self.alphabet:
+                    c_index = alphabet.index(character)
+                    new_char = alphabet[(c_index - shift) % 26]
+                    decrypted += new_char
+                else:
+                    decrypted += character
+            if decrypted in data:
+                found_words.append(decrypted)
+        return found_words
 
-        if not found_words:
-            print("No matching words found.")
-    except KeyboardInterrupt:
-        print('\nexiting')
-    except Exception as e:
-        print(f"Error: {e}")
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+from utils.encrypt import Encrypt
+from utils.decrypt import Decrypt
+print("hi")
+word_l = (Decrypt("krwod").decrypt())
+word = "".join(i for i in word_l)
+print(word)
